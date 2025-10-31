e1627d7c98f139ea77e28ad358ac9ab8b2a2e012
william prince <williamprince9000@gmail.com>
2025-10-31 15:13:26 -0400

    Testing vim setting and conform changed from prettier to cl

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/System/Vector2.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+  window.setFramerateLimit(144);
+  sf::Vector2f rectV = sf::Vector2f(2, 2);
+  sf::RectangleShape rectShape = sf::RectangleShape(rectV);
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+    window.clear();
+    window.display();
+  }
 }
